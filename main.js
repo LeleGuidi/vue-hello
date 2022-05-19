ed34d1fd91c836c62be08aaaa65e9d7dec30d2c3
LeleGuidi <emanuele.guidi98@outlook.it>
2022-05-19 15:08:08 +0200

    Aggiunto messaggio tramite v-model

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const app = new Vue({
-    el: `#root`
+    el: `#root`,
+    data: {
+        message: `Ciao a tutti!`,
+        img: `https://images.pushsquare.com/436862c3d36d3/cyberpunk-2077-ps5-ps4.large.jpg`
+    }
 })
